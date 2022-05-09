18634a0aaed97558c7011eca5b1ec488d6d5f6e4
jfraz027 <jfrazier027@gmail.com>
2022-05-09 19:53:29 -0400

    Foriegn key update

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -19,32 +19,37 @@ CREATE TABLE employees (
     last_name VARCHAR   NOT NULL,
     sex VARCHAR   NOT NULL,
     hire_date DATE   NOT NULL,
-    PRIMARY KEY (emp_no)
+    PRIMARY KEY (emp_no),
+    FOREIGN KEY(emp_title_id) REFERENCES titles (title_id);
 );
 
 CREATE TABLE departments (
     dept_no VARCHAR   NOT NULL,
     dept_name VARCHAR   NOT NULL,
-    PRIMARY KEY (dept_no)
+    PRIMARY KEY (dept_no)    
 );
 
 CREATE TABLE dept_manager (
     dept_no VARCHAR   NOT NULL,
     emp_no int   NOT NULL,
     PRIMARY KEY (dept_no,emp_no)
+    FOREIGN KEY(dept_no) REFERENCES departments (dept_no);
+    FOREIGN KEY(emp_no) REFERENCES employees (emp_no);
 );
 
 CREATE TABLE dept_emp (
     emp_no int   NOT NULL,
     dept_no VARCHAR   NOT NULL,
-    PRIMARY KEY (
-        emp_no,dept_no)
+    PRIMARY KEY (emp_no,dept_no)
+    FOREIGN KEY(emp_no) REFERENCES employees (emp_no);
+    FOREIGN KEY(dept_no) REFERENCES departments (dept_no);
 );
 
 CREATE TABLE salaries (
     emp_no int   NOT NULL,
     salary int   NOT NULL,
-    PRIMARY KEY (emp_no)
+    PRIMARY KEY (emp_no),
+    FOREIGN KEY(emp_no) REFERENCES employees (emp_no);
 );
 
 ALTER TABLE employees ADD CONSTRAINT fk_employees_emp_title_id FOREIGN KEY(emp_title_id)
