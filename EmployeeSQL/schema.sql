bb9f78cf18f978460c90f1b6256b94d868d1b138
jfraz027 <jfrazier027@gmail.com>
2022-05-12 11:21:11 -0400

    Add Query png

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -1,9 +1,5 @@
--- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+-- Table schema 
 
--- Modify this code to update the DB schema diagram.
--- To reset the sample schema, replace everything with
--- two dots ('..' - without quotes).
 
 CREATE TABLE titles (
     title_id VARCHAR   NOT NULL,
@@ -52,20 +48,4 @@ CREATE TABLE salaries (
     FOREIGN KEY(emp_no) REFERENCES employees (emp_no)
 );
 
---ALTER TABLE employees ADD CONSTRAINT fk_employees_emp_title_id FOREIGN KEY(emp_title_id)
---REFERENCES titles (title_id);
 
---ALTER TABLE dept_manager ADD CONSTRAINT fk_dept_manager_dept_no FOREIGN KEY(dept_no)
---REFERENCES departments (dept_no);
-
---ALTER TABLE dept_manager ADD CONSTRAINT fk_dept_manager_emp_no FOREIGN KEY(emp_no)
---REFERENCES employees (emp_no);
-
---ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_emp_emp_no FOREIGN KEY(emp_no)
---REFERENCES employees (emp_no);
-
---ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_emp_dept_no FOREIGN KEY(dept_no)
---REFERENCES departments (dept_no);
-
---ALTER TABLE salaries ADD CONSTRAINT fk_salaries_emp_no FOREIGN KEY(emp_no)
---REFERENCES employees (emp_no);
